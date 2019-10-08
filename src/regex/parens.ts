e65dc1cbd081452e3c1bc1bfcdf2970b1cc94211
Joey Gauthier <digitaljedi@outlook.com>
2019-10-08 08:37:55 -0500

    figured out how to add example to docs

--- src/regex/parens.ts
+++ src/regex/parens.ts
@@ -2,6 +2,13 @@
 /**
  * Takes string and returns string with parenthesis and contain between parenthesis removed
  *
+ * Basic usage example:
+ * ```js
+ * const parens = require('stringman').parens; // or `import {parens} from 'stringman'`;
+ * const noParens = parens.remove('this will come back (and this will be removed)');
+ * console.log('noParens'); // 'this will come back'
+ * ```
+ * 
  * @param {string} str
  * @returns {string}
  */
