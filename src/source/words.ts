220e2f66ac1da139cc34197dd9745cc68cb2d198
Joey Gauthier <digitaljedi@outlook.com>
2019-10-25 06:52:55 -0500

    added comments to words modules with examples

--- src/source/words.ts
+++ src/source/words.ts
@@ -6,6 +6,15 @@ const wordsSplitChars = /(\.|,|\?| |-|!)/gm;
 /**
  * Takes 2 strings and returns a number for the number of times the second string exists in the first string; optionally takes third boolean argument to make case sensitive
  *
+ * Basic usage example:
+ * ```js
+ * import {words} from 'stringman'; // or const words = require('stringman').words;
+ * const insensitive = words.specificWordCount('Maybe-this will, trip it up! Will it? We WILL see.', 'will');
+ * const sensitive = words.specificWordCount('Maybe-this will, trip it up! Will it? We WILL see.', 'will', true);
+ * console.log(insensitive); // 3
+ * console.log(sensitive); // 1
+ * ```
+ *
  * @param {string} str
  * @param {string} word
  * @returns {number}
@@ -30,6 +39,15 @@ function specificWordCount(str: string, word: string, strict?: boolean): number 
 
 /**
  * Takes a string and returns the number of words in the string as a number
+ *
+ * Basic usage example:
+ * ```js
+ * import {words} from 'stringman'; // or const words = require('stringman').words;
+ * const count = words.wordCount('This sentence has 4 words.');
+ * const countAgain = words.wordCount('This sentence has five words.');
+ * console.log(count); // 4
+ * console.log(countAgain); // 5
+ * ```
  *
  * @param {string} str
  * @returns {number}
